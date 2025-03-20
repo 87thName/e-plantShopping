cc77b2c1df83b2123ccea4be6800011532cb6efb
87thName <you@example.com>
2025-03-20 10:12:52 -0400

    Added functionalities

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -8,28 +8,36 @@ const CartItem = ({ onContinueShopping }) => {
   const dispatch = useDispatch();
 
   // Calculate total amount for all products in the cart
-  const calculateTotalAmount = () => {
+    const calculateTotalAmount = () => {
+    return cart.reduce((total, item) => total + Number(item.cost.substring(1)) * item.quantity, 0);
  
   };
 
   const handleContinueShopping = (e) => {
-   
+    onContinueShopping(e);
+
   };
 
-
-
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {
-   
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
   };
 
   const handleRemove = (item) => {
+        dispatch(removeItem(item));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    return Number(item.cost.substring(1)) * item.quantity;
+
+  };
+
+  const checkoutAlert = () => {
+    alert("Coming soon!");
   };
 
   return (
@@ -57,12 +65,11 @@ const CartItem = ({ onContinueShopping }) => {
       <div className="continue_shopping_btn">
         <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button className="get-started-button1" onClick={checkoutAlert}>Checkout</button>
       </div>
     </div>
   );
 };
 
 export default CartItem;
-
 
